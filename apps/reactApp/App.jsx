49e3fa1078647732b94274dae44cd1efb234a514
Gavin Francis <gav@eh7.co.uk>
2023-05-14 23:06:03 +0100

    add scaffold abi function proof on viability for a react application able to work with smart contract comunication, configuration and interaction

---
+++ apps/reactApp/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+//import { render } from 'react-dom';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>React App in DEV</h1>
+        <a href="../index.html">home</a>
+        <a href="../wallet.html">wallet</a>
+      </div>
+    )
+  }
+}
