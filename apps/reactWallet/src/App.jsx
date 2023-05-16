b014e15a6510184c1599007f6a6981f9177a0099
Gavin Francis <gav@eh7.co.uk>
2023-05-16 13:26:17 +0100

    add scaffold reactWallet app and build/deploy command

---
+++ apps/reactWallet/src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+//import { render } from 'react-dom';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Wallet App</h1>
+        <ul>
+          <li>
+            <a href="../../index.html">home</a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
