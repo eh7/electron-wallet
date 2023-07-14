16b6fa29c1e92004f6a16f21ee83515fc909c939
Gavin Francis <gav@eh7.co.uk>
2023-07-14 14:44:00 +0100

    wip: adding simple html metamsk to send tx subscribe and publisher interface. Adding local storage for txs data associate with address

--- examples/ethersHtml/syncTxs.js
+++ examples/ethersHtml/syncTxs.js
@@ -10,50 +10,52 @@ const rpcURL = 'https://polygon-mumbai.gateway.tenderly.co';
 //const provider = new ethers.providers.JsonRpcProvider();
 const provider = new ethers.JsonRpcProvider(rpcURL);
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const run = async () => {
   const myAddr = subscribeAddress;
+  //let currentBlock =  37925391 ;
   let currentBlock =  await provider.getBlockNumber(); 
-  const n = await provider.getTransactionCount(subscribeAddress);
-  const bal = (await provider.getBalance(subscribeAddress)).toString();
-  console.log('run', myAddr, currentBlock, n, bal);
+  let n = await provider.getTransactionCount(subscribeAddress);
+  let bal = (await provider.getBalance(subscribeAddress)).toString();
+  //console.log('run', myAddr, currentBlock, n, bal);
+
+  let found = 0;
 
   for (var i=currentBlock; i >= 0 && (n > 0 || bal > 0); --i) {
     try {
-      //console.log(i);
-      //var block = await provider.getTransaction(i-1);
       var block = await provider.getBlock(i);
-      console.log(block.transactions.length);
+      console.log(i, block.transactions.length);
       let txCount = 0;
-      block.transactions.forEach(function(e) {
-        const tx = await provider.getTransaction(e);
+      for (const txs of block) {
+        const tx = await provider.getTransaction(txs);
+        /*
         console.log(
           txCount,
-          e,
+          txs,
           tx,
         );
+        */
+        if (myAddr == tx.to) {
+          found++;
+          console.log(
+            '(myAddr == tx.to) :: \n',
+            i,
+            '\nfrom:', tx.from,
+            '\nto:', tx.to,
+            '\nvalue:', tx.value.toString(10),
+            '\ndata:', ethers.toUtf8String(
+              tx.data,
+            ),
+            '\n', n, found
+          );
+        }
         txCount++;
-      });
-      
-      if (i === currentBlock - 0) process.exit();
-
-/*
-      var block = eth.getBlock(i, true);
-      if (block && block.transactions) {
-        block.transactions.forEach(function(e) {
-          if (myAddr == e.from) {
-            if (e.from != e.to)
-              bal = bal.plus(e.value);
-            console.log(i, e.from, e.to, e.value.toString(10));
-            --n;
-          }
-          if (myAddr == e.to) {
-            if (e.from != e.to)
-              bal = bal.minus(e.value);
-            console.log(i, e.from, e.to, e.value.toString(10));
-          }
-        });
       }
-*/
+      await sleep(2000);
+      //if (i === currentBlock - 100) process.exit();
     } catch (e) {
       console.error("Error in block " + i, e);
       process.exit();
