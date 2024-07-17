40399510c273d8774539d9bacc3675cd427d33d4
Gavin Francis <gav@eh7.co.uk>
2024-07-17 21:58:38 +0100

    adding bnasic example to call dht api fom wallet app, and add some functionality so you can navigate to and from wallet app from electron home

--- apps/wallet/src/pages/TestAPI.js
+++ apps/wallet/src/pages/TestAPI.js
@@ -19,7 +19,9 @@ export default class TestAPI extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      messages: {}
+    };
 
     pageSetup();
   }
@@ -29,17 +31,39 @@ export default class TestAPI extends React.Component {
     const host = "http://127.0.0.1:5000"
     const result = await axios.get(host + '/messages')
     console.log('messages', result)
+    this.setState({ messages: result })
+  }
+
+  componentDidMount(){
+    this.getMessages()
   }
 
   render() {
-
-    this.getMessages()
-
     return (
       <>
         <h1>Test node API</h1>
         <p>
           tesing the dht node api
+          <div>
+            {this.state.messages.data &&
+              <ul>
+                {
+                  this.state.messages.data.map((item, index) => {
+                    return (
+                      <li>
+                        <>{index}}</> 
+                        {Object.keys(item).map((key, index) => {
+                          return (
+                            <p>{key}: {item[key]}</p>
+                          )
+                        })}
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            }
+          </div>
         </p>
       </>
     );
